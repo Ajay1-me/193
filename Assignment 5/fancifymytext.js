7d081b7d42294e0758c268ca7322090d97a29b2b
Ajaydeep Singh <deepajay0713@gmail.com>
2025-05-04 23:46:49 -0700

    Adding files

---
+++ Assignment 5/fancifymytext.js
@@ -0,0 +1,36 @@
+function makeTextBigger() {
+	alert("Hello, world!");
+	document.getElementById("textInput").style.fontSize = "24pt";
+  }
+  
+  function changeStyle() {
+	const textArea = document.getElementById("textInput");
+	const fancy = document.getElementById("fancyShmancy").checked;
+  
+	if (fancy) {
+	  textArea.style.fontWeight = "bold";
+	  textArea.style.color = "blue";
+	  textArea.style.textDecoration = "underline";
+	} else {
+	  textArea.style.fontWeight = "normal";
+	  textArea.style.color = "black";
+	  textArea.style.textDecoration = "none";
+	}
+  }
+  
+  function mooText() {
+	let textArea = document.getElementById("textInput");
+	let text = textArea.value.toUpperCase();
+  
+	let sentences = text.split(".");
+	for (let i = 0; i < sentences.length - 1; i++) {
+	  let words = sentences[i].trim().split(" ");
+	  if (words.length > 0) {
+		words[words.length - 1] += "-Moo";
+		sentences[i] = words.join(" ");
+	  }
+	}
+  
+	textArea.value = sentences.join(". ");
+  }
+  
